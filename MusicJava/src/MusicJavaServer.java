b7f26c455b3402a3e2acfcb71aa8bd01592916ff
Matthew <n0650950@my.ntu.ac.uk>
2018-04-07 18:18:18 +0100

    Started server

---
+++ MusicJava/src/MusicJavaServer.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.io.*;
+
+public class MusicJavaServer {
+    public static void main(String[] args) throws IOException {
+        
+        ServerSocket server = new ServerSocket(9090);
+        
+        
+        
+        while (true) {
+            System.out.println("Waiting for connection...");
+            Socket client = server.accept();
+            System.out.println("Connected" + client.getInetAddress());
+            //assign each client to a thread
+         }
+    }
+}
